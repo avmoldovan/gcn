e9d90ac33ce54c618fd8c1108a626b0a30df61b0
avmoldovan <adrian.moldovan@gmail.com>
2023-11-21 17:32:32 +0200

    multiple runs and new gnn intro script

---
+++ gcn/GNN-intro.py
@@ -0,0 +1,102 @@
+import os
+import torch
+os.environ['TORCH'] = torch.__version__
+print(torch.__version__)
+#ensure that the PyTorch and the PyG are the same version
+
+# !pip install -q torch-scatter -f https://data.pyg.org/whl/torch-${TORCH}.html
+# !pip install -q torch-sparse -f https://data.pyg.org/whl/torch-${TORCH}.html
+# !pip install -q git+https://github.com/pyg-team/pytorch_geometric.git
+# !pip install -q torch-geometric -f https://data.pyg.org/whl/torch-${TORCH}.html
+
+# Helper function for visualization.
+#%matplotlib inline
+import networkx as nx
+import matplotlib.pyplot as plt
+
+def visualize_graph(G, color):
+    plt.figure(figsize=(7,7))
+    plt.xticks([])
+    plt.yticks([])
+    nx.draw_networkx(G, pos=nx.spring_layout(G, seed=42), with_labels=False,
+                     node_color=color, cmap="Set2")
+    plt.show()
+
+from torch_geometric.datasets import KarateClub
+
+dataset = KarateClub()
+
+print('Dataset properties')
+print('==============================================================')
+print(f'Dataset: {dataset}') #This prints the name of the dataset
+print(f'Number of graphs in the dataset: {len(dataset)}')
+print(f'Number of features: {dataset.num_features}') #Number of features each node in the dataset has
+print(f'Number of classes: {dataset.num_classes}') #Number of classes that a node can be classified into
+
+
+#Since we have one graph in the dataset, we will select the graph and explore it's properties
+
+data = dataset[0]
+print('Graph properties')
+print('==============================================================')
+
+# Gather some statistics about the graph.
+print(f'Number of nodes: {data.num_nodes}') #Number of nodes in the graph
+print(f'Number of edges: {data.num_edges}') #Number of edges in the graph
+print(f'Average node degree: {data.num_edges / data.num_nodes:.2f}') # Average number of nodes in the graph
+print(f'Contains isolated nodes: {data.has_isolated_nodes()}') #Does the graph contains nodes that are not connected
+print(f'Contains self-loops: {data.has_self_loops()}') #Does the graph contains nodes that are linked to themselves
+print(f'Is undirected: {data.is_undirected()}') #Is the graph an undirected graph
+
+
+from torch_geometric.utils import to_networkx
+
+G = to_networkx(data, to_undirected=True)
+visualize_graph(G, color=data.y)
+
+import torch
+from torch.nn import Linear
+from torch_geometric.nn import GCNConv
+
+
+class GCN(torch.nn.Module):
+    def __init__(self):
+        super(GCN, self).__init__()
+        torch.manual_seed(12345)
+        self.conv1 = GCNConv(dataset.num_features, 4)
+        self.conv2 = GCNConv(4, 4)
+        self.conv3 = GCNConv(4, 2)
+        self.classifier = Linear(2, dataset.num_classes)
+
+    def forward(self, x, edge_index):
+        h = self.conv1(x, edge_index)
+        h = h.tanh()
+        h = self.conv2(h, edge_index)
+        h = h.tanh()
+        h = self.conv3(h, edge_index)
+        h = h.tanh()  # Final GNN embedding space.
+
+        # Apply a final (linear) classifier.
+        out = self.classifier(h)
+
+        return out, h
+
+model = GCN()
+print(model)
+
+model = GCN()
+criterion = torch.nn.CrossEntropyLoss()  #Initialize the CrossEntropyLoss function.
+optimizer = torch.optim.Adam(model.parameters(), lr=0.01)  # Initialize the Adam optimizer.
+
+def train(data):
+    optimizer.zero_grad()  # Clear gradients.
+    out, h = model(data.x, data.edge_index)  # Perform a single forward pass.
+    loss = criterion(out[data.train_mask], data.y[data.train_mask])  # Compute the loss solely based on the training nodes.
+    loss.backward()  # Derive gradients.
+    optimizer.step()  # Update parameters based on gradients.
+    return loss, h
+
+for epoch in range(401):
+    loss, h = train(data)
+    print(f'Epoch: {epoch}, Loss: {loss}')
+
